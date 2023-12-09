dad563fb86d738c7022f54a6004bc8d84113473e
LWR <lwr1104@qq.com>
2023-12-09 21:49:52 +0800

    docs: Optimized some comment

--- starbot_datasource/__init__.py
+++ starbot_datasource/__init__.py
@@ -1,7 +1,2 @@
-import platform
-
 from .core.event import *
 from .core.datasource import *
-
-if 'windows' in platform.system().lower():
-    asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
